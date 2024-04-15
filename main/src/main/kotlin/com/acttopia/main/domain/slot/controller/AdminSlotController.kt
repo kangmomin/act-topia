4c39ce5257ab56c79ff8c6fee0ef5ac3ced6d0c2
koldin108902 <alex108902@naver.com>
2024-04-16 00:24:55 +0900

    Feat: 원하는 유저의 슬롯 확인

--- main/src/main/kotlin/com/acttopia/main/domain/slot/controller/AdminSlotController.kt
+++ main/src/main/kotlin/com/acttopia/main/domain/slot/controller/AdminSlotController.kt
@@ -1,9 +1,12 @@
 package com.acttopia.main.domain.slot.controller
 
+import com.acttopia.main.domain.slot.controller.response.SlotInfoResponse
 import com.acttopia.main.domain.slot.service.CommandSlotService
+import com.acttopia.main.domain.slot.service.QuerySlotService
 import com.acttopia.main.global.common.basic.response.BasicResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/admin/slot")
 class AdminSlotController(
-    private val commandSlotService: CommandSlotService
+    private val commandSlotService: CommandSlotService,
+    private val querySlotService: QuerySlotService
 ) {
 
     @DeleteMapping("/delete/{slotId}")
@@ -21,5 +25,23 @@ class AdminSlotController(
         commandSlotService.deleteSlot(slotId)
 
         return BasicResponse.ok("슬롯을 삭제하였습니다.")
+    }
+
+    @GetMapping("/{userId}/list")
+    fun getUsersSlots(
+        @PathVariable userId: Long
+    ): ResponseEntity<BasicResponse.BaseResponse> {
+        val slots = querySlotService.mySlots(userId)
+
+        val response = slots.map {
+            SlotInfoResponse(
+                slotId = it.id!!,
+                midValue = it.midValue!!,
+                subKeyword = it.subKeyword!!,
+                mainKeyword = it.mainKeyword!!
+            )
+        }
+
+        return BasicResponse.ok(response)
     }
 }
